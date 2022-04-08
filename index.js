98a0edeb66f4e478b23542fdff1d244f83acf929
Subhadip Sarkar <87298461+Subhahdip@users.noreply.github.com>
2022-04-08 21:40:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.ceil(Math.random() * 6)
+var randomNumber2 = Math.ceil(Math.random() * 6)
+
+var randomDice1 = "images/dice" + randomNumber1 + ".png"
+var randomDice2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDice1)
+document.querySelectorAll("img")[1].setAttribute("src", randomDice2)
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins🥳";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🥳";
+}
+else
+    document.querySelector("h1").innerHTML = "Its a Draw🤪";
